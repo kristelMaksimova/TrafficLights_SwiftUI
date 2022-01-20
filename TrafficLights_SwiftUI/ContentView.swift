f9ddb50b42a27aa60e99d951b8a207f636eb60e6
Maksimova Kristel <idmaksimovak@gmail.com>
2022-01-20 13:50:59 +0300

    Initial Commit

---
+++ TrafficLights_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLights_SwiftUI
+//
+//  Created by Kristi on 20.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLights_SwiftUI/TrafficLights_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLights_SwiftUIApp.swift
+//  TrafficLights_SwiftUI
+//
+//  Created by Kristi on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLights_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
